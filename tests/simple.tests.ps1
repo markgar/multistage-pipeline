139d2cf954ec7f575c79467aba8ffc024d45bbce
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-05-21 21:46:48 -0500

    remove

--- tests/simple.tests.ps1
+++ tests/simple.tests.ps1
@@ -2,11 +2,6 @@ Param(
     [string] $TemplateFilePath
 )
 
-$json = Get-Content -Path $TemplateFilePath
-$template = ConvertFrom-Json $json
-
-Write-Host $template
-
 Describe 'Basic Pester Tests' {
     It 'A test that should be true' {
         $true | Should -Be $true
