3a898b52969428edb231a2d298faa4185767b7bc
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-05-21 22:10:31 -0500

    add debugging

--- tests/simple.tests.ps1
+++ tests/simple.tests.ps1
@@ -6,6 +6,8 @@ Write-Host $TemplateFilePath
 $template = Get-Content -Path $TemplateFilePath | Out-String | ConvertFrom-Json
 
 Write-Host $template.resources[0].type
+Write-Debug $template.resources[0].type
+Write-Output $template.resources[0].type
 
 Describe 'Basic Pester Tests' {
     It 'A test that should be true' {
