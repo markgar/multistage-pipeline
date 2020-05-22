e9351f604a2a7d4ebe63e68f84e8b7ad130711f7
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-05-21 21:01:50 -0500

    add load of arm template

--- tests/simple.tests.ps1
+++ tests/simple.tests.ps1
@@ -1,3 +1,13 @@
+Param(
+    [string] [Parameter(Mandatory = $true)] $TemplateFile,
+    [hashtable] [Parameter(Mandatory = $true)] $Parameters
+)
+
+$json = Get-Content -Path $TemplateFile
+$template = ConvertFrom-Json $json
+
+Write-Host $template
+
 Describe 'Basic Pester Tests' {
     It 'A test that should be true' {
         $true | Should -Be $false
