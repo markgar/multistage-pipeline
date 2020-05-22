fa2ba047a188684c2983bccb5a9774bcc635a5e3
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-05-21 22:07:12 -0500

    debug

--- tests/simple.tests.ps1
+++ tests/simple.tests.ps1
@@ -2,8 +2,10 @@ Param(
     [string] $TemplateFilePath
 )
 
+Write-Host $TemplateFilePath
 $template = Get-Content -Path $TemplateFilePath | Out-String | ConvertFrom-Json
 
+Write-Host $template.resources[0].type
 
 Describe 'Basic Pester Tests' {
     It 'A test that should be true' {
