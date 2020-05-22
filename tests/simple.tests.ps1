7110334ca78ea449c3c8effbbad0fd75d7919dca
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-05-21 21:51:21 -0500

    add

--- tests/simple.tests.ps1
+++ tests/simple.tests.ps1
@@ -2,6 +2,8 @@ Param(
     [string] $TemplateFilePath
 )
 
+$json = Get-Content -Path $TemplateFilePath
+
 Describe 'Basic Pester Tests' {
     It 'A test that should be true' {
         $true | Should -Be $true
