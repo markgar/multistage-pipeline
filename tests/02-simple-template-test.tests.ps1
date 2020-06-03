297e957046d905908a8919bf0f02cfd90da9a1d9
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-06-03 15:40:56 -0500

    more debugging

--- tests/02-simple-template-test.tests.ps1
+++ tests/02-simple-template-test.tests.ps1
@@ -5,7 +5,9 @@ $template = Get-Content -Path $TemplateFileName | Out-String | ConvertFrom-Json
 Write-Host $template.resources.Count
 
 Describe 'Sample Pester Test of an ARM Template' {
+    Write-Host $template.resources.Count
     It 'Has a total of 2 objects in the resources array' {
+        Write-Host $template.resources.Count
         $template.resources.Count | Should -Be 2
     }
 }
