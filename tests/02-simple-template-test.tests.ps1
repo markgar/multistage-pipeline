39b7843aac55b124899efb2774b82a80700c259d
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-06-03 15:44:43 -0500

    ?

--- tests/02-simple-template-test.tests.ps1
+++ tests/02-simple-template-test.tests.ps1
@@ -6,8 +6,11 @@ Write-Host $template.resources.Count
 
 Describe 'Sample Pester Test of an ARM Template' {
     Write-Host $template.resources.Count
+    $count = $template.resource.Count
+    Write-Host $count
     It 'Has a total of 2 objects in the resources array' {
         Write-Host $template.resources.Count
+        Write-Host $count
         $template.resources.Count | Should -Be 2
     }
 }
