8b98d628aeb8cbc5f707ed97fa51c4d4f4571207
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-06-03 15:32:32 -0500

    change sample test

--- tests/02-simple-template-test.tests.ps1
+++ tests/02-simple-template-test.tests.ps1
@@ -4,9 +4,10 @@ $content = Get-Content -Path $TemplateFileName
 Write-Host $content
 $template = Get-Content -Path $TemplateFileName | Out-String | ConvertFrom-Json
 Write-Host $template
+Write-Host $template.resources.Count
 
 Describe 'Sample Pester Test of an ARM Template' {
     It 'Has a resource title of Microsoft.Network/virtualNetworks' {
-        $template.resources[0].type | Should -Be 'Microsoft.Network/virtualNetworks'
+        $template.resources.Count | Should -Be 5
     }
 }
