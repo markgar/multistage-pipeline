b7f36ab8a39b462907d5f1e447154ba7edd30eeb
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-06-03 15:52:17 -0500

    more debugging

--- tests/02-simple-template-test.tests.ps1
+++ tests/02-simple-template-test.tests.ps1
@@ -2,15 +2,15 @@ $TemplateFileName = [System.Environment]::GetEnvironmentVariable('TemplateFileNa
 
 $template = Get-Content -Path $TemplateFileName | Out-String | ConvertFrom-Json
 
-Write-Host $template.resources.Count
+Write-Host "1: " + $template.resources.Count
 
 Describe 'Sample Pester Test of an ARM Template' {
-    Write-Host $template.resources.Count
+    Write-Host "2: " + $template.resources.Count
     $count = $template.resource.Count
-    Write-Host $count
+    Write-Host "3: " + $count
     It 'Has a total of 2 objects in the resources array' {
-        Write-Host $template.resources.Count
-        Write-Host $count
+        Write-Host "4: " + $template.resources.Count
+        Write-Host "5: " + $count
         $template.resources.Count | Should -Be 2
     }
 }
