4c55e900d19fbf9aac44c7dd5df9398c9030004b
Mark Garner <markgarner@kabhardw-dev2.fareast.corp.microsoft.com>
2020-06-03 15:36:53 -0500

    fix test

--- tests/02-simple-template-test.tests.ps1
+++ tests/02-simple-template-test.tests.ps1
@@ -1,13 +1,10 @@
 $TemplateFileName = [System.Environment]::GetEnvironmentVariable('TemplateFileName')
-Write-Host $TemplateFileName
-$content = Get-Content -Path $TemplateFileName
-Write-Host $content
+
 $template = Get-Content -Path $TemplateFileName | Out-String | ConvertFrom-Json
-Write-Host $template
-Write-Host $template.resources.Count
+
 
 Describe 'Sample Pester Test of an ARM Template' {
-    It 'Has a resource title of Microsoft.Network/virtualNetworks' {
-        $template.resources.Count | Should -Be 5
+    It 'Has a total of 2 objects in the resources array' {
+        $template.resources.Count | Should -Be 2
     }
 }
