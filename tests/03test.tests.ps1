f9b98445e2682349ecdff990f981107959ac1d94
Mark Garner <markgarner@WU2TEHPV07CX-04.Redmond.corp.microsoft.com>
2020-06-04 11:15:12 -0500

    debug

--- tests/03test.tests.ps1
+++ tests/03test.tests.ps1
@@ -7,12 +7,16 @@
 
 Describe '03-Describe Block' {
     $TemplateFileName = [System.Environment]::GetEnvironmentVariable('TemplateFileName')
+    Write-Host "1:" + $TemplateFileName
 
     $template = Get-Content -Path $TemplateFileName | Out-String | ConvertFrom-Json
+    Write-Host "2:" + $template
 
     foreach ($resource in $template.resources)
     {
+        Write-Host "3:" + $resource
         It 'Has a resource location of eastus' {
+            Write-Host "4:" + $resource
             $resource.location | Should -Be 'eastus'
         }
     }
