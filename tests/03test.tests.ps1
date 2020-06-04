c217751c48542b79e1f1d8d75929eeb59caf7dd4
Mark Garner <markgarner@WU2TEHPV07CX-04.Redmond.corp.microsoft.com>
2020-06-04 11:21:09 -0500

    syntax fix

--- tests/03test.tests.ps1
+++ tests/03test.tests.ps1
@@ -12,12 +12,13 @@ Describe '03-Describe Block' {
     $template = Get-Content -Path $TemplateFileName | Out-String | ConvertFrom-Json
     Write-Host "2:" + $template
 
-    for ($i=0; $i -lt $template.resources.count; i++)
+    for ($i=0; $i -lt $template.resources.count; $i++)
     {
         Write-Host "3:" + $resource
         It 'Has a resource location of eastus' {
             Write-Host "4:" + $resource
             Write-Host "5: " + $template
+            Write-Host "5: " + $i
             $template.resources[$i].location | Should -Be 'eastus'
         }
     }
