336fba1aeceac5bb3a96e837e9c41338eea025cb
Mark Garner <markgarner@WU2TEHPV07CX-04.Redmond.corp.microsoft.com>
2020-06-04 11:11:41 -0500

    move out of beforeall

--- tests/03test.tests.ps1
+++ tests/03test.tests.ps1
@@ -1,11 +1,15 @@
-BeforeAll {
-    $TemplateFileName = [System.Environment]::GetEnvironmentVariable('TemplateFileName')
+# BeforeAll {
+#     $TemplateFileName = [System.Environment]::GetEnvironmentVariable('TemplateFileName')
 
-    $template = Get-Content -Path $TemplateFileName | Out-String | ConvertFrom-Json
-}
+#     $template = Get-Content -Path $TemplateFileName | Out-String | ConvertFrom-Json
+# }
 
 
 Describe '03-Describe Block' {
+    $TemplateFileName = [System.Environment]::GetEnvironmentVariable('TemplateFileName')
+
+    $template = Get-Content -Path $TemplateFileName | Out-String | ConvertFrom-Json
+
     foreach ($resource in $template.resources)
     {
         It 'Has a resource location of eastus' {
